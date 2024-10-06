4cf6c25935a6957ed4e4969c58752b7ab54da4d7
Alabi Temitope <t.banji@rocketmail.com>
2024-10-06 16:50:34 +0100

    updated

--- src/route/index.js
+++ src/route/index.js
@@ -4,7 +4,6 @@ import NoMatch from '../components/notfound/NotFound';
 import Home from '../components/home/Home';
 import About from '../components/aboutus/AboutUs';
 import Contact from '../components/contact/Contact';
-import Service from '../components/service/Services';
 import Installation from '../components/service/installation/Installation';
 import Maintenance from '../components/service/maintenance/Maintenance';
 import Repair from '../components/service/repair/Repair';
@@ -16,29 +15,30 @@ import HumanResource from '../components/service/hr/HumanResource';
 
 import CallCenter from '../components/service/callcenter/CallCenter';
 import OtherServices from '../components/service/otherservices/OtherServices';
+import Services from '../components/service/service/Services';
 
 const Routing = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/products" component={Products} />
+      <Route exact path="/" component={<Home />} />
+      <Route path="/about" component={<About />} />
+      <Route path="/contact" component={<Contact />} />
+      <Route path="/products" component={<Products />} />
       <Route
         path="/products-glazed_porcelain_tile"
-        component={Glazed_porcelain_tile}
+        component={<Glazed_porcelain_tile />}
       />
       <Route
         path="/products-rustic_porcelain_tile"
-        component={Rustic_porcelain_tile}
+        component={<Rustic_porcelain_tile />}
       />
-      <Route path="/services" component={Service} />
-      <Route path="/services-installation" component={Installation} />
-      <Route path="/services-maintenance" component={Maintenance} />
-      <Route path="/services-repair" component={Repair} />
-      <Route path="/human-resource-outsourcing" component={HumanResource} />
-      <Route path="/callcenter-solution" component={CallCenter} />
-      <Route path="/other-business-services" component={OtherServices} />
+      <Route path="/services" component={<Services />} />
+      <Route path="/services-installation" component={<Installation />} />
+      <Route path="/services-maintenance" component={<Maintenance />} />
+      <Route path="/services-repair" component={<Repair />} />
+      <Route path="/human-resource-outsourcing" component={<HumanResource />} />
+      <Route path="/callcenter-solution" component={<CallCenter />} />
+      <Route path="/other-business-services" component={<OtherServices />} />
       <Route component={NoMatch} />
     </Switch>
   );
